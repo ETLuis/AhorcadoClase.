069857c60abd4ab713f8710703a3dbe44c02b7d4
jorge y luis <lcampanogoldar@gmail.com>
2022-03-13 23:32:39 +0100

    Update 1.2 Ahorcado

---
+++ src/Fichero.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Scanner;
+
+public class Fichero {
+
+/*
+    public static void crearPalabra(List<Character> jugadores){
+        File fichero = new File("C:\\Users\\joerge y luis\\Desktop\\prueba.txt");
+        try{
+            Scanner leer = new Scanner(fichero);
+            while (leer.hasNextLine()){
+                jugadores.add(new Character(leer.next(), leer.nextInt(), leer.nextInt(), leer.nextInt()));
+
+            }
+            leer.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+*/
+}
